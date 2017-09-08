aef7c04386d4fd8e1d6461390e3fda3e98b868f0
Simon Oulevay <simon.oulevay@gmail.com>
2017-09-08 14:46:11 +0200

    Per-environment configuration files

---
+++ config/optimization.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const useDefaultConfig = require('@ionic/app-scripts/config/optimization.config.js');
+
+// See https://github.com/gshigeto/ionic-environment-variables
+module.exports = function () {
+  useDefaultConfig.resolve.alias = {
+    "@app/env": path.resolve(`./src/environments/environment${process.env.IONIC_ENV == 'prod' ? '' : '.' + process.env.IONIC_ENV}.ts`)
+  };
+
+  return useDefaultConfig;
+};
