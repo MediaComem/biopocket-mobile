0b02c0292dc5b8f8f6019f3042aa2945053ebe51
Simon Oulevay <simon.oulevay@gmail.com>
2018-10-31 16:04:44 +0100

    Update src/components/bip-menu-header/bip-menu-header.stub.ts
    Co-Authored-By: Tazaf <Tazaf@users.noreply.github.com>

--- src/components/bip-menu-header/bip-menu-header.stub.ts
+++ src/components/bip-menu-header/bip-menu-header.stub.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 /**
- * This stub component should be use in tests where another component's template uses `bip-menu-header` elements.
+ * This stub component should be used in tests where another component's template uses `bip-menu-header` elements.
  * Import this class and add it to the `declarations` array.
  * If your test needs to stub multiple components, you should probably import the `StubComponentsModule` instead and add it to the `imports` array.
  */
