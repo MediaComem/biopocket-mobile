adb89b71b2b17e2450a586762b0022f504674afa
Simon Oulevay <simon.oulevay@gmail.com>
2018-10-31 16:05:31 +0100

    Update src/components/bip-profile-picture/bip-profile-picture.stub.ts
    Co-Authored-By: Tazaf <Tazaf@users.noreply.github.com>

--- src/components/bip-profile-picture/bip-profile-picture.stub.ts
+++ src/components/bip-profile-picture/bip-profile-picture.stub.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 /**
  * This stub component should be used in tests where another component's template uses `bip-profile-picture` elements.
- * Import this class and add it to the `declaration` array.
+ * Import this class and add it to the `declarations` array.
  * If your test needs to stub multiple components, you should probably import the `StubComponentsModule` instead and add it to the `import` array.
  */
 @Component({ selector: 'bip-profile-picture', template: '' })
