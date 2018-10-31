eef25fb846bacbb80943e6259214195d58ff7f93
Simon Oulevay <simon.oulevay@gmail.com>
2018-10-31 16:05:10 +0100

    Update src/components/bip-menu-item-icon/bip-menu-item-icon.stub.ts
    Co-Authored-By: Tazaf <Tazaf@users.noreply.github.com>

--- src/components/bip-menu-item-icon/bip-menu-item-icon.stub.ts
+++ src/components/bip-menu-item-icon/bip-menu-item-icon.stub.ts
@@ -5,7 +5,7 @@ import { BipIconStub } from '@components/bip-icon/bip-icon.stub';
 /**
  * This stub component should be used in tests where another component's template uses `bip-menu-item-icon` elements.
  * Import this class and add it to the `declarations` array.
- * If your test needs to stub multiple components, you should probably import the `StubComponentsModule` instead and add it to the `import` array.
+ * If your test needs to stub multiple components, you should probably import the `StubComponentsModule` instead and add it to the `imports` array.
  */
 @Component({ selector: 'bip-menu-item-icon', template: '' })
 export class BipMenuItemIconStub extends BipIconStub {
