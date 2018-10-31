722cde58c2b783a952998a9671aae67f3ed464cc
Simon Oulevay <simon.oulevay@gmail.com>
2018-10-31 15:57:32 +0100

    Update src/components/bip-icon/bip-icon.stub.ts
    Co-Authored-By: Tazaf <Tazaf@users.noreply.github.com>

--- src/components/bip-icon/bip-icon.stub.ts
+++ src/components/bip-icon/bip-icon.stub.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 /**
  * This stub component should be use in tests where another component's template uses `bip-icon` elements.
- * Import this class and add it to the `declaration` array.
+ * Import this class and add it to the `declarations` array.
  * If your test needs to stub multiple components, you should probably import the `StubComponentsModule` instead and add it to the `import` array.
  */
 @Component({ selector: 'bip-icon', template: '' })
