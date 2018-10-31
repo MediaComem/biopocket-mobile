4185b5ac2ec70d8b68c588bf6d7fe75899c9006c
Simon Oulevay <simon.oulevay@gmail.com>
2018-10-31 16:03:29 +0100

    Update src/components/bip-menu-item/bip-menu-item.stub.ts
    Co-Authored-By: Tazaf <Tazaf@users.noreply.github.com>

--- src/components/bip-menu-item/bip-menu-item.stub.ts
+++ src/components/bip-menu-item/bip-menu-item.stub.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import MenuItemIcon from '@classes/menu-item-icon.class';
 
 /**
- * This stub component should be use in tests where another component's template uses `bip-menu-item` elements.
+ * This stub component should be used in tests where another component's template uses `bip-menu-item` elements.
  * Import this class and add it to the `declarations` array.
  * If your test needs to stub multiple components, you should probably import the `StubComponentsModule` instead and add it to the `import` array.
  */
