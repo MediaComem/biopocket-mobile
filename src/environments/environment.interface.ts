3dc550a43ac04931385241a29c6d7d13edfb3705
Mathias Oberson <mathias.oberson@heig-vd.ch>
2018-12-17 14:24:16 +0100

    Should fix Travis tests...

--- src/environments/environment.interface.ts
+++ src/environments/environment.interface.ts
@@ -3,7 +3,9 @@
  * When updating the content of the ENV variable, this interface should be updated before anything else.
  * This way, the project build should fail if you forgot to update any environment file or the interface of the EnvService.
  */
-export interface EnvInterface {
+interface EnvInterface {
   backendUrl: string;
   environment: string;
 }
+
+export default EnvInterface;
